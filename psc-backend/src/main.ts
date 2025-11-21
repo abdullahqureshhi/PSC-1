0bd0cbb30bbdbeaa0f144b5ab74ca6ddd5f7e352
Atif-Muhammad <mohatif02@gmail.com>
2025-11-21 22:18:18 +0500

    123

--- psc-backend/src/main.ts
+++ psc-backend/src/main.ts
@@ -6,19 +6,18 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    // origin: (origin, callback) => {
-    //   const allowed = [
-    //     'https://psc-mu.vercel.app',
-    //     'http://localhost:5173',
-    //   ];
+    origin: (origin, callback) => {
+      const allowed = [
+        'https://psc.up.railway.app',
+        'http://localhost:5173',
+      ];
 
-    //   if (!origin || allowed.includes(origin)) {
-    //     callback(null, true);
-    //   } else {
-    //     callback(new Error('Not allowed by CORS'));
-    //   }
-    // },
-    origin: ['https://psc.up.railway.app/'],
+      if (!origin || allowed.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Client-Type'],
     credentials: true,
